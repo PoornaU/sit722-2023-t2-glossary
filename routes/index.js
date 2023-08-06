63710af041a33dd7a153e788cce0ea618880f14f
PoornaU <62684793+PoornaU@users.noreply.github.com>
2023-08-06 11:10:45 +1000

    Added Task 3.2P

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/', function(req, res, next) {
+
   fs.readFile('glossary.json', (err, data) => {
+
     if (err) throw err;
+
     let glossary = JSON.parse(data);
+
     res.render('index', { title: 'SIT722 DevOps Glossary', glossary: glossary });
+    
   });
 });
 
